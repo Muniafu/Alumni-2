1c536237e0bca8d3ab1dcbe4f2ed0fcb6ce6477c
Muniafu <elambaraka2019@gmail.com>
2025-09-11 18:08:54 +0300

    `Removed RegisteredUserController and related routes, updated other routes accordingly`

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AlumniController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\ApprovalController;
-use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\JobPostingController;
@@ -20,17 +19,14 @@ use App\Http\Controllers\MentorshipController;
 use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\ReportController;
 
-
 /*
 |--------------------------------------------------------------------------
 | Public Routes
 |--------------------------------------------------------------------------
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 
 // Public directory & events (limited info)
 Route::get('/public/directory', [UserSearchController::class, 'publicIndex'])->name('public.directory');
@@ -41,11 +37,6 @@ Route::get('/public/events', [EventController::class, 'publicIndex'])->name('pub
 | Guest Routes (Auth)
 |--------------------------------------------------------------------------
 */
-Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
-    Route::post('register', [RegisteredUserController::class, 'store']);
-});
-
 Route::get('/pending-approval', [ApprovalController::class, 'pending'])->name('pending-approval');
 
 /*
@@ -60,13 +51,13 @@ Route::middleware(['auth', 'approved'])->group(function () {
 
     /*
     |-----------------------------
-    | Profile Management (All Roles)
+    | Profile Management
     |-----------------------------
     */
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::post('/profile/details', [ProfileController::class, 'updateDetails'])->name('profile.details.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::post('/profile/details', [ProfileController::class, 'updateDetails'])->name('profile.details.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     /*
     |-----------------------------
@@ -74,7 +65,7 @@ Route::middleware(['auth', 'approved'])->group(function () {
     |-----------------------------
     */
     Route::resource('events', EventController::class)->except(['show']);
-    Route::get('/events/{event}', [EventController::class, 'show'])->name('events.show'); // Public view for registered users
+    Route::get('/events/{event}', [EventController::class, 'show'])->name('events.show');
     Route::get('/events/calendar', [EventController::class, 'calendar'])->name('events.calendar');
     Route::get('/events/calendar/data', [EventController::class, 'getCalendarEvents'])->name('events.calendar.data');
     Route::post('/events/{event}/rsvp', [EventController::class, 'rsvp'])->name('events.rsvp');
@@ -88,18 +79,21 @@ Route::middleware(['auth', 'approved'])->group(function () {
     Route::middleware(['can:create jobs'])->group(function () {
         Route::resource('jobs', JobPostingController::class)->except(['index', 'show']);
     });
-    Route::get('/jobs/create', [JobPostingController::class, 'create'])->name('jobs.create');
-    Route::post('/jobs', [JobPostingController::class, 'store'])->name('jobs.store');
-    Route::get('jobs', [JobPostingController::class, 'index'])->name('jobs.index');Route::get('/jobs/my-postings', [JobPostingController::class, 'myPostings'])
-            ->name('jobs.my-posting')
-            ->middleware('role:alumni');
-    Route::get('jobs/{job}', [JobPostingController::class, 'show'])->name('jobs.show');
+
+    Route::get('/jobs', [JobPostingController::class, 'index'])->name('jobs.index');
+    Route::get('/jobs/{job}', [JobPostingController::class, 'show'])->name('jobs.show');
     Route::post('/jobs/{job}/apply', [JobPostingController::class, 'apply'])->name('jobs.apply');
-    Route::get('/jobs/{job}/applications', [JobPostingController::class, 'applications'])->middleware('role:admin|alumni')->name('jobs.applications');
-    Route::put('/applications/{application}', [JobPostingController::class, 'updateApplicationStatus'])->middleware('role:admin|alumni')->name('applications.update');
-    Route::get('/{application}', [JobPostingController::class, 'showApplication'])->name('jobs.applications.show');
-    Route::put('/{application}', [JobPostingController::class, 'updateApplicationStatus'])->name('jobs.applications.update');
-    Route::get('/', [JobPostingController::class, 'applications'])->name('jobs.applications');
+    Route::get('/jobs/my-postings', [JobPostingController::class, 'myPostings'])
+        ->middleware('role:alumni')
+        ->name('jobs.my-postings');
+    Route::get('/jobs/{job}/applications', [JobPostingController::class, 'applications'])
+        ->middleware('role:admin|alumni')
+        ->name('jobs.applications');
+    Route::put('/applications/{application}', [JobPostingController::class, 'updateApplicationStatus'])
+        ->middleware('role:admin|alumni')
+        ->name('applications.update');
+    Route::get('/applications/{application}', [JobPostingController::class, 'showApplication'])
+        ->name('applications.show');
 
     /*
     |-----------------------------
@@ -108,14 +102,6 @@ Route::middleware(['auth', 'approved'])->group(function () {
     */
     Route::resource('conversations', ConversationController::class)->only(['index', 'create', 'store', 'show', 'destroy']);
     Route::resource('conversations.messages', MessageController::class)->only(['store', 'destroy']);
-    Route::get('/conversations', [ConversationController::class, 'index'])->name('conversations.index');
-    Route::get('/conversations/create', [ConversationController::class, 'create'])->name('conversations.create');
-    Route::post('/conversations', [ConversationController::class, 'store'])->name('conversations.store');
-    Route::get('/conversations/{conversation}', [ConversationController::class, 'show'])->name('conversations.show');
-    Route::delete('/conversations/{conversation}', [ConversationController::class, 'destroy'])->name('conversations.destroy');
-
-    Route::post('/conversations/{conversation}/messages', [MessageController::class, 'store'])->name('messages.store');
-    Route::delete('/messages/{message}', [MessageController::class, 'destroy'])->name('messages.destroy');
 
     /*
     |-----------------------------
@@ -128,30 +114,18 @@ Route::middleware(['auth', 'approved'])->group(function () {
         Route::resource('threads', ForumThreadController::class)->except(['index']);
         Route::post('/threads/{thread}/posts', [ForumPostController::class, 'store'])->name('posts.store');
         Route::resource('posts', ForumPostController::class)->only(['edit', 'update', 'destroy']);
-        Route::post('/forum/threads/{thread}/subscribe', [ForumThreadController::class, 'subscribe'])
-        ->name('forum.threads.subscribe')
-        ->middleware('auth');
-        Route::post('/forum/threads/{thread}/unsubscribe', [ForumThreadController::class, 'unsubscribe'])
-        ->name('forum.threads.unsubscribe')
-        ->middleware('auth');
-        });
+        Route::post('/threads/{thread}/subscribe', [ForumThreadController::class, 'subscribe'])->name('threads.subscribe');
+        Route::post('/threads/{thread}/unsubscribe', [ForumThreadController::class, 'unsubscribe'])->name('threads.unsubscribe');
+    });
 
     /*
     |-----------------------------
-    | Notification routes
+    | Notifications
     |-----------------------------
     */
-    Route::post('/notifications/mark-as-read/{id}', [NotificationController::class, 'markAsRead'])
-        ->name('notifications.markAsRead')
-        ->middleware('auth');
-
-    Route::get('/notifications', [NotificationController::class, 'index'])
-        ->name('notifications.all')
-        ->middleware('auth');
-
-    Route::post('/notifications/mark-all-read', [NotificationController::class, 'markAllAsRead'])
-        ->name('notifications.markAllAsRead')
-        ->middleware('auth');
+    Route::get('/notifications', [NotificationController::class, 'index'])->name('notifications.all');
+    Route::post('/notifications/mark-as-read/{id}', [NotificationController::class, 'markAsRead'])->name('notifications.markAsRead');
+    Route::post('/notifications/mark-all-read', [NotificationController::class, 'markAllAsRead'])->name('notifications.markAllAsRead');
 
     /*
     |-----------------------------
@@ -159,14 +133,14 @@ Route::middleware(['auth', 'approved'])->group(function () {
     |-----------------------------
     */
     Route::prefix('mentorship')->name('mentorship.')->group(function () {
-        Route::get('/', [MentorshipController::class, 'index'])->name('mentorship.index');
-        Route::get('/find', [MentorshipController::class, 'findMentors'])->name('mentorship.find');
-        Route::get('/mentor/{mentor}', [MentorshipController::class, 'showMentor'])->name('mentorship.show-mentor');
-        Route::get('/{mentorship}', [MentorshipController::class, 'show'])->name('mentorship.show');
-        Route::post('/request/{mentor}', [MentorshipController::class, 'sendRequest'])->name('mentorship.send-request');
-        Route::get('/requests', [MentorshipController::class, 'myRequests'])->name('mentorship.requests');
-        Route::post('/requests/{mentorshipRequest}/respond', [MentorshipController::class, 'respondToRequest'])->name('mentorship.respond-to-request');
-        Route::put('/{mentorship}', [MentorshipController::class, 'update'])->name('mentorship.update');
+        Route::get('/', [MentorshipController::class, 'index'])->name('index');
+        Route::get('/find', [MentorshipController::class, 'findMentors'])->name('find');
+        Route::get('/mentor/{mentor}', [MentorshipController::class, 'showMentor'])->name('show-mentor');
+        Route::get('/{mentorship}', [MentorshipController::class, 'show'])->name('show');
+        Route::post('/request/{mentor}', [MentorshipController::class, 'sendRequest'])->name('send-request');
+        Route::get('/requests', [MentorshipController::class, 'myRequests'])->name('requests');
+        Route::post('/requests/{mentorshipRequest}/respond', [MentorshipController::class, 'respondToRequest'])->name('respond-to-request');
+        Route::put('/{mentorship}', [MentorshipController::class, 'update'])->name('update');
     });
 
     /*
@@ -179,14 +153,6 @@ Route::middleware(['auth', 'approved'])->group(function () {
 
     /*
     |-----------------------------
-    | Engagement (Leaderboards, Badges)
-    |-----------------------------
-
-    Route::get('/leaderboard', [EngagementController::class, 'leaderboard'])->name('leaderboard');
-    */
-
-    /*
-    |-----------------------------
     | Admin Routes
     |-----------------------------
     */
@@ -196,7 +162,7 @@ Route::middleware(['auth', 'approved'])->group(function () {
         Route::post('/approve-user/{user}', [AdminController::class, 'approveUser'])->name('approve-user');
         Route::delete('/reject-user/{user}', [AdminController::class, 'rejectUser'])->name('reject-user');
 
-        // Permissions Manangement
+        // Permissions
         Route::get('/permissions', [AdminController::class, 'permissions'])->name('permissions');
         Route::get('/permissions/{role}', [AdminController::class, 'getRolePermissions'])->name('permissions.get');
         Route::post('/permissions/update', [AdminController::class, 'updatePermissions'])->name('permissions.update');
@@ -214,27 +180,7 @@ Route::middleware(['auth', 'approved'])->group(function () {
             Route::get('/', [ReportController::class, 'index'])->name('index');
             Route::post('/generate', [ReportController::class, 'generate'])->name('generate');
         });
-
-        // Announcements & Newsletter
-        /*
-        Route::get('/announcements', [AnnouncementController::class, 'index'])->name('announcements.index');
-        Route::post('/announcements', [AnnouncementController::class, 'store'])->name('announcements.store');
-        Route::delete('/announcements/{announcement}', [AnnouncementController::class, 'destroy'])->name('announcements.destroy');
-        */
     });
-
-    /*
-    |-----------------------------
-    | Moderator Routes
-    |-----------------------------
-
-    Route::middleware(['role:moderator'])->prefix('moderator')->name('moderator.')->group(function () {
-        Route::get('/dashboard', [ModeratorController::class, 'dashboard'])->name('dashboard');
-        Route::get('/reports', [ModeratorController::class, 'viewReports'])->name('reports');
-        Route::post('/reports/{report}/resolve', [ModeratorController::class, 'resolveReport'])->name('reports.resolve');
-    });
-
-    */
 
     /*
     |-----------------------------
